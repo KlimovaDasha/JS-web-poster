30e730663e39c184d5d3403186e50b1cec767d9f
Klimova Dasha <89785723+KlimovaDasha@users.noreply.github.com>
2022-03-31 00:33:27 +0300

    Add files via upload

---
+++ javaskripts/java-script.js
@@ -0,0 +1,277 @@
+// FIRST PATH
+$( document ).ready(function(){
+	  $( ".button_12" ).click(function(){
+	    $( ".path_black_1" ).addClass( "animation_paths", 1000 );
+	    $( ".path_white_1" ).addClass( "animation_paths", 1000 );
+	  });
+    });
+
+$( document ).ready(function(){
+	  $( ".button_13" ).click(function(){
+			$( ".path_black_1" ).removeClass( "animation_paths", 1000 );
+	    $( ".path_white_1" ).removeClass( "animation_paths", 1000 );
+	  });
+    });
+
+$( document ).ready(function(){
+      $(".button_11").on( "click", function() {
+        $(".marquee-infinite").toggleClass( "newClass", 1000 );
+      });
+    });
+
+$( document ).ready(function(){
+      $(".button_11").on( "click", function() {
+        $(".first_path_1").toggleClass( "newerClass", 1000 );
+      });
+    });
+
+// BACKGROUND FOR BUTTONS
+// $( document ).ready(function(){
+//       $(".button_11").on( "click", function() {
+//         $(this).toggleClass("borderclass", 1000);
+//       });
+//     });
+//
+// $( document ).ready(function(){
+//       $(".button_12").on( "click", function() {
+//         $(this).toggleClass( "bog", 1000 );
+//       });
+//     });
+//
+// $( document ).ready(function(){
+//       $(".button_13").on( "click", function() {
+//         $(this).toggleClass("bug", 1000);
+//       });
+//     });
+// BACKGROUND FOR BUTTONS
+
+// FIRST PATH
+
+
+// SECOND PATH
+$( document ).ready(function(){
+	  $( ".button_22" ).click(function(){
+	    $( ".second_path_1" ).animate({left: "-=100%"}, 15500);
+	    $( ".second_path_white" ).animate({left: "-=100%"}, 15500);
+	  });
+    });
+
+$( document ).ready(function(){
+	  $( ".button_23" ).click(function(){
+	    $( ".second_path_1" ).stop();
+	    $( ".second_path_white" ).stop();
+	  });
+    });
+
+$( document ).ready(function(){
+      $(".button_21").on( "click", function() {
+        $(".music_visual_2").toggleClass( "newClass", 1000 );
+      });
+    });
+
+$( document ).ready(function(){
+      $(".button_21").on( "click", function() {
+        $(".second_path_1").toggleClass( "newerClass", 1000 );
+      });
+    });
+
+// BACKGROUND FOR BUTTONS
+$( document ).ready(function(){
+      $(".button_21").on( "click", function() {
+        $(this).toggleClass("borderclass", 1000);
+      });
+    });
+
+$( document ).ready(function(){
+      $(".button_22").on( "click", function() {
+        $(this).toggleClass( "bog", 1000 );
+      });
+    });
+
+$( document ).ready(function(){
+      $(".button_23").on( "click", function() {
+        $(this).toggleClass("bug", 1000);
+      });
+    });
+// SECOND PATH
+
+// THIRD PATH
+$( document ).ready(function(){
+	  $( ".button_32" ).click(function(){
+	    $( ".third_path_1" ).animate({left: "-=100%"}, 15500);
+	    $( ".third_path_white" ).animate({left: "-=100%"}, 15500);
+	  });
+    });
+
+$( document ).ready(function(){
+	  $( ".button_33" ).click(function(){
+	    $( ".third_path_1" ).stop();
+	    $( ".third_path_white" ).stop();
+	  });
+    });
+
+$( document ).ready(function(){
+      $(".button_31").on( "click", function() {
+        $(".music_visual_3").toggleClass( "newClass", 1000 );
+      });
+    });
+
+$( document ).ready(function(){
+      $(".button_31").on( "click", function() {
+        $(".third_path_1").toggleClass( "newerClass", 1000 );
+      });
+    });
+
+// BACKGROUND FOR BUTTONS
+$( document ).ready(function(){
+      $(".button_31").on( "click", function() {
+        $(this).toggleClass("borderclass", 1000);
+      });
+    });
+
+$( document ).ready(function(){
+      $(".button_32").on( "click", function() {
+        $(this).toggleClass( "bog", 1000 );
+      });
+    });
+
+$( document ).ready(function(){
+      $(".button_33").on( "click", function() {
+        $(this).toggleClass("bug", 1000);
+      });
+    });
+// THIRD PATH
+
+// FOURTH PATH
+$( document ).ready(function(){
+	  $( ".button_42" ).click(function(){
+	    $( ".fourth_path_1" ).animate({left: "-=100%"}, 15500);
+	    $( ".fourth_path_white" ).animate({left: "-=100%"}, 15500);
+	  });
+    });
+
+$( document ).ready(function(){
+	  $( ".button_43" ).click(function(){
+	    $( ".fourth_path_1" ).stop();
+	    $( ".fourth_path_white" ).stop();
+	  });
+    });
+
+$( document ).ready(function(){
+      $(".button_41").on( "click", function() {
+        $(".music_visual_4").toggleClass( "newClass", 1000 );
+      });
+    });
+
+$( document ).ready(function(){
+      $(".button_41").on( "click", function() {
+        $(".fourth_path_1").toggleClass( "newerClass", 1000 );
+      });
+    });
+
+// BACKGROUND FOR BUTTONS
+$( document ).ready(function(){
+      $(".button_41").on( "click", function() {
+        $(this).toggleClass("borderclass", 1000);
+      });
+    });
+
+$( document ).ready(function(){
+      $(".button_42").on( "click", function() {
+        $(this).toggleClass( "bog", 1000 );
+      });
+    });
+
+$( document ).ready(function(){
+      $(".button_43").on( "click", function() {
+        $(this).toggleClass("bug", 1000);
+      });
+    });
+// FOURTH PATH
+
+$( document ).ready(function(){
+      $(".synth").on( "click", function() {
+        $(".circle1").toggleClass( "stopclass", 1000 );
+				$(".circle2").toggleClass( "stopclass", 1000 );
+				$(".circle3").toggleClass( "stopclass", 1000 );
+				$(".circle4").toggleClass( "stopclass", 1000 );
+				$(".circle5").toggleClass( "stopclass", 1000 );
+				$(".circle6").toggleClass( "stopclass", 1000 );
+				$(".graphic_1").toggleClass( "stop2class", 1000 );
+				$(".graphic_2").toggleClass( "stop2class", 1000 );
+      });
+    });
+
+$( document ).ready(function(){
+      $(".synth_text").on( "click", function() {
+        $(".circle1").toggleClass( "stopclass", 1000 );
+				$(".circle2").toggleClass( "stopclass", 1000 );
+				$(".circle3").toggleClass( "stopclass", 1000 );
+				$(".circle4").toggleClass( "stopclass", 1000 );
+				$(".circle5").toggleClass( "stopclass", 1000 );
+				$(".circle6").toggleClass( "stopclass", 1000 );
+				$(".graphic_1").toggleClass( "stop2class", 1000 );
+				$(".graphic_2").toggleClass( "stop2class", 1000 );
+      });
+    });
+
+		$( document ).ready(function(){
+					$( ".make_bc" ).click(function(){
+						$( ".newton_cradle" ).removeClass( "paused", 1000 );
+						// $( ".newton_cradle" ).removeClass( "makeblue", 1000 );
+					  // $( ".newton_cradle" ).addClass( "newpaused", 1000 );
+						// $( ".newton_cradle" ).addClass( "moverunning", 1000 );
+					});
+					});
+
+					$( document ).ready(function(){
+								$( ".make_bc" ).click(function(){
+									// $( ".newton_cradle" ).removeClass( "paused", 1000 );
+									$( ".newton_cradle" ).addClass( "makeblue", 1000 );
+									// $( ".newton_cradle" ).addClass( "newpaused", 1000 );
+									// $( ".newton_cradle" ).addClass( "moverunning", 1000 );
+								});
+								});
+
+		$( document ).ready(function(){
+			$( ".make_ac" ).click(function(){
+				$( ".newton_cradle" ).addClass( "paused", 1000 );
+			  // $( ".newton_cradle" ).addClass( "makeblue", 1000 );
+				// $(".newton_cradle").css({'transform':'rotate(-25deg)'});
+			});
+		  });
+
+			$( document ).ready(function(){
+				$( ".make_ac" ).click(function(){
+					$(".newton_cradle").css({'transform':'rotate(-25deg)'});
+				});
+			  });
+
+			// 	var count = 1;
+			//  upperchange.onclick = function pluscount(){
+			//    count++
+			//    num.innerHTML = count;
+			//  }
+			// lowerchange.onclick =  function minuscount(){
+			//    count--
+			//    num.innerHTML = count;
+			//  }
+			//  pic.onclick =  function pluscount(){
+ 			//    count++
+ 			//    num.innerHTML = count;
+ 			//  }
+
+
+
+       
+			 // var score=0;
+			 //
+			 // $("#upperchange").click(function(){
+			 // score++;
+			 // $('#num').text(score);
+			 // });
+			 //
+			 // $("#lowerchange").click(function(){
+			 // score--;
+			 // $('#num').text(score);
+			 // });
